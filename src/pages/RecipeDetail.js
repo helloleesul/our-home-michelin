1b033919a4b4e45eb6ed3cd021c5d3518d7cad41
[SW5] 이슬 <suerish.e@gmail.com>
2023-08-18 18:06:42 +0900

    comment: 내 냉장고 모달 기능 임시 작업용 주석 수정

--- src/pages/RecipeDetail.js
+++ src/pages/RecipeDetail.js
@@ -4,12 +4,12 @@ import PortalModal from "../components/common/PortalModal";
 import MyFridge from "../components/MyFridge";
 
 function RecipeDetail(props) {
-  // 냉장고 컴포넌트(버튼, 모달)은 생성되면 App의 라우터나 Layout에 추가할 예정, 또는 필요한 페이지컴포넌트마다 불러올 예정
+  // 내 냉장고 컴포넌트(버튼, 모달)은 생성되면 App의 라우터나 Layout에 추가할 예정, 또는 필요한 페이지컴포넌트마다 불러올 예정
   const [showModal, setShowModal] = useState(false);
 
   return (
     <div>
-      {/* 임시 작업용 버튼 */}
+      {/* 내 냉장고 임시 작업용 버튼 및 모달 */}
       <button onClick={() => setShowModal(true)}>냉장고 버튼 예시</button>
       <PortalModal handleShowModal={showModal} size="md">
         <MyFridge onClose={() => setShowModal(false)} />
