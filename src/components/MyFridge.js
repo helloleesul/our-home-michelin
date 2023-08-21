b91751f75f6ef21e0b2a513fa9c26c40a9cff338
[SW5] 이슬 <suerish.e@gmail.com>
2023-08-22 03:36:36 +0900

    style: 내 냉장고 퍼블리싱 작업중

--- src/components/MyFridge.js
+++ src/components/MyFridge.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import CloseIcon from "../assets/CloseIcon";
-import { Header, Content } from "./MyFridge.style";
+import {
+  Header,
+  Content,
+  EmptyFridge,
+  Fridge,
+  IngredientList,
+} from "./MyFridge.style";
+import fridgeImg from "../assets/img/emptyFridge.svg";
 
 MyFridge.defaultProps = {
   fridgeData: [
@@ -26,6 +33,50 @@ MyFridge.defaultProps = {
 };
 
 function MyFridge({ onClose, fridgeData }) {
+  const [ingredientAdder, setIngredientAdder] = useState(false);
+  const [IngredientData, setIngredientData] = useState([
+    {
+      type: "육류",
+      ingredient: [
+        {
+          name: "닭고기",
+          selected: false,
+          imgUrl: "",
+        },
+        {
+          name: "돼지고기",
+          selected: false,
+          imgUrl: "",
+        },
+        {
+          name: "소고기",
+          selected: false,
+          imgUrl: "",
+        },
+      ],
+    },
+    {
+      type: "야채류",
+      ingredient: [
+        {
+          name: "가지",
+          selected: false,
+          imgUrl: "",
+        },
+        {
+          name: "고추",
+          selected: false,
+          imgUrl: "",
+        },
+        {
+          name: "양파",
+          selected: false,
+          imgUrl: "",
+        },
+      ],
+    },
+  ]);
+
   return (
     <>
       <Header>
@@ -35,10 +86,38 @@ function MyFridge({ onClose, fridgeData }) {
         </button>
       </Header>
       <Content>
-        {fridgeData?.length !== 0 ? (
-          <p>냉장고 식재료 채워졌음</p>
+        {ingredientAdder ? (
+          <IngredientList>
+            {IngredientData.map((group, index) => {
+              return (
+                <div key={index}>
+                  <h5>{group.type}</h5>
+                  <ul>
+                    {group.ingredient.map((item, index) => {
+                      return <li key={index}>{item.name}</li>;
+                    })}
+                  </ul>
+                </div>
+              );
+            })}
+            <button onClick={() => setIngredientAdder(false)}>돌아가기</button>
+            <button onClick={() => setIngredientAdder(false)}>추가완료</button>
+          </IngredientList>
+        ) : fridgeData?.length === 0 ? (
+          <Fridge>냉장고 식재료 채워졌음</Fridge>
         ) : (
-          <p>냉장고 식재료 비었음</p>
+          <EmptyFridge>
+            <img src={fridgeImg} alt="빈 식재료" />
+            <h4>냉장고가 비었어요!</h4>
+            <p>
+              재료를 추가하고
+              <br />
+              바로 만들 수 있는 레시피를 확인 해보세요!
+            </p>
+            <button onClick={() => setIngredientAdder(true)}>
+              재료 추가하기
+            </button>
+          </EmptyFridge>
         )}
       </Content>
     </>
--- src/components/MyFridge.style.js
+++ src/components/MyFridge.style.js
@@ -27,3 +27,36 @@ export const Header = styled.header`
 export const Content = styled.div`
   ${commonStyle}
 `;
+
+export const IngredientList = styled.div``;
+export const Fridge = styled.div``;
+
+export const EmptyFridge = styled.div`
+  color: #464646;
+  text-align: center;
+  img {
+    margin: 0 auto;
+    display: block;
+    padding-right: 40px;
+  }
+  h4 {
+    font-size: 24px;
+    margin: 20px 0 30px;
+    font-weight: 700;
+  }
+  p {
+    font-size: 18px;
+    line-height: 1.3;
+    margin-bottom: 40px;
+  }
+  button {
+    background: #f7411f;
+    border: none;
+    font-size: 16px;
+    color: #fff;
+    border-radius: 10px;
+    padding: 8px 50px;
+    cursor: pointer;
+    margin-bottom: 10px;
+  }
+`;
