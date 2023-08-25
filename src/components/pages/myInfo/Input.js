80acc5af14937c90816ceaa30227986e8e6b5e50
[SW5] 김석민 <gudwp815@gmail.com>
2023-08-26 01:03:16 +0900

    feat:회원정보 수정 페이지 기능추가

--- src/components/pages/myInfo/Input.js
+++ src/components/pages/myInfo/Input.js
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import * as S from "./Input.style";
 
 function Input(props) {
-  const { text, type, placeholder, showBtn, onChange, value } = props;
+  const {
+    text,
+    type,
+    placeholder,
+    showBtn,
+    onChange,
+    value,
+    onBtnClick,
+    readOnly,
+  } = props;
   const [btnText, setBtnText] = useState("변경하기");
-  const [readOnly, setReadOnly] = useState(true);
-  const [showInput, setShowInput] = useState(false);
-
+  const [read, setRead] = useState(readOnly);
   const handleButtonClick = () => {
-    setReadOnly((readOnly) => !readOnly);
     setBtnText((btnText) => (btnText === "변경하기" ? "임시저장" : "변경하기"));
+    setRead(!read);
     if (text === "비밀번호") {
-      alert("비밀번호 변경 버튼 클릭");
-      setShowInput(true);
+      onBtnClick();
+      setRead(!read);
     }
   };
 
@@ -23,18 +30,12 @@ function Input(props) {
         <S.Input
           type={type}
           placeholder={placeholder}
-          readOnly={readOnly}
+          readOnly={read}
           onChange={onChange}
           value={value}
         ></S.Input>
         {showBtn && <S.Button onClick={handleButtonClick}>{btnText}</S.Button>}
       </S.InputContainer>
-      {showInput && (
-        <S.InputContainer>
-          <S.Label>{text} 확인</S.Label>
-          <S.Input type={type}></S.Input>
-        </S.InputContainer>
-      )}
     </>
   );
 }
--- src/pages/MyInfo.js
+++ src/pages/MyInfo.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import * as S from "../components/pages/UserAccessForm/UserAccessForm.style";
 import Input from "../components/pages/myInfo/Input";
@@ -11,7 +12,9 @@ function MyInfo(props) {
   const [nickname, setNickname] = useState("");
   const [userEmail, setUserEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showConfirmPassword, setShowConfirmPassword] = useState("");
+  const navigate = useNavigate();
   useEffect(() => {
     (async () => {
       try {
@@ -36,6 +39,14 @@ function MyInfo(props) {
       alert("비밀번호는 6글자 이상이어야 합니다.");
       return;
     }
+    if (password !== confirmPassword) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+    const saveConfrim = window.confirm("저장 하시겠습니까?");
+    if (!saveConfrim) {
+      return;
+    }
     const userData = {
       nickName: nickname,
       email: userEmail,
@@ -44,11 +55,17 @@ function MyInfo(props) {
 
     try {
       const response = await axios.put("/api/myinfo", userData);
-      console.log(response.data);
+      if (response.data) {
+        alert("회원정보 수정 완료");
+        navigate("/mypage");
+      }
     } catch (error) {
       console.error("에러", error);
       console.log(error.response.data.message);
     }
+  };
+  const handlePasswordClick = () => {
+    setShowConfirmPassword(true);
   };
 
   return (
@@ -63,24 +80,37 @@ function MyInfo(props) {
           <Input
             text="닉네임"
             type="text"
-            showBtn="true"
+            showBtn
             value={nickname}
             onChange={(event) => setNickname(event.target.value)}
             placeholder="닉네임은 2글자 이상이어야 합니다."
+            readOnly={true}
           ></Input>
           <Input
             text="이메일"
             type="text"
             value={userEmail}
             onChange={(event) => setUserEmail(event.target.value)}
+            readOnly={true}
           />
           <Input
             text="비밀번호"
             type="password"
-            showBtn="true"
+            showBtn
             onChange={(event) => setPassword(event.target.value)}
-            placeholder="비밀번호는 6글자 이상이어야 합니다."
+            placeholder="비밀번호는 6글자 이상 입력해주세요."
+            onBtnClick={handlePasswordClick}
+            readOnly={true}
           />
+          {showConfirmPassword && (
+            <Input
+              text="비밀번호 확인"
+              type="password"
+              onChange={(event) => setConfirmPassword(event.target.value)}
+              placeholder="비밀번호는 6글자 이상 입력해주세요."
+              readOnly={false}
+            />
+          )}
           <S.Text></S.Text>
           <S.Btn onClick={handleSaveClick}>저장</S.Btn>
           <S.Text fontSize="13px" onClick={() => setShowModal(true)}>
