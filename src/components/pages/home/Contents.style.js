eccbc2864c0ceb1ac801fbf7be2206319aafed90
enkomyo <122080137+enkomyo@users.noreply.github.com>
2023-08-25 11:36:43 +0900

    FEAT:레시피연동추가,냉장고버튼수정

--- src/components/pages/home/Contents.style.js
+++ src/components/pages/home/Contents.style.js
@@ -1,7 +1,6 @@
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 
-
 export const Section = styled.div`
   width: 1000px;
   display: flex;
@@ -9,13 +8,20 @@ export const Section = styled.div`
   align-items: center;
   text-align: center;
   box-sizing: border-box;
-  margin: 20px 0;
+  margin: 15px 0;
   cursor: pointer;
+
+  p {
+    width: 100%;
+    height: 20%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 `;
 
 export const FoodImage = styled.img`
-  width: 176px;
-  height: 132px;
+  width: 100%;
+  height: 80%;
   object-fit: cover;
   margin-bottom: 10px;
 `;
@@ -23,4 +29,6 @@ export const FoodImage = styled.img`
 export const RecipeLink = styled(Link)`
   text-decoration: none;
   color: #464646;
+  width: 176px;
+  height: 172px;
 `;
