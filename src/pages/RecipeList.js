482b431e2e4dee0b119bd0702857a06232e33d62
[SW5] 김경연 <argand0304@gmail.com>
2023-08-29 11:36:43 +0900

    feat:title

--- src/pages/RecipeList.js
+++ src/pages/RecipeList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MAIN_THEME_COLOR } from "../libs/const/color";
 // import { FillHeart, StrokeHeart } from "./assets/HeartIcon.js";
 // import LikeBtn from "../components/common/LikeBtn";
@@ -6,27 +6,22 @@ import { MAIN_THEME_COLOR } from "../libs/const/color";
 import List from "../components/pages/recipeList/List";
 import * as S from "./RecipeList.style";
 
-
 function RecipeList(props) {
   // const [like, setLike] = useState(false);
+  const [recipes, setRecipes] = useState([]);
 
   return (
-
     <>
       {/* 모든 레시피  :: 레시피DB ID의 갯수를 변수로 만들면 되나? */}
-      <S.Title><h3>전체 레시피</h3></S.Title>
+      <S.Title>
+        <h3>{props.title || "전체 레시피"}</h3>
+      </S.Title>
 
       <S.Lists>
-
         <List /> <List /> <List /> <List /> <List />
-
       </S.Lists>
     </>
-
-
   );
 }
-
-
 
 export default RecipeList;
