f83083a394bf0d550d94b633a4c9ab38efa02983
[SW5] 이슬 <suerish.e@gmail.com>
2023-08-29 19:10:58 +0900

    feat: useAuthStatus 커스텀 훅 추가

--- src/components/MyFridge.js
+++ src/components/MyFridge.js
@@ -15,6 +15,7 @@ import requestApi from "../libs/const/api";
 import { Link } from "react-router-dom";
 import Calendar from "./common/Calendar";
 import PortalModal from "./common/PortalModal";
+import useAuthStatus from "../libs/hooks/useAuthStatus";
 
 function formatDate(dateString) {
   const options = { year: "numeric", month: "2-digit", day: "2-digit" };
@@ -28,9 +29,8 @@ function formatDate(dateString) {
   return `${year}년 ${month}월 ${day}일`;
 }
 
-function MyFridge({ onClose, isAuth }) {
-  // 회원 인증 확인
-  const [isUser, setIsUser] = useState(isAuth);
+function MyFridge({ onClose }) {
+  const { isAuth } = useAuthStatus();
 
   // 기본 식재료 추가 모드
   const [ingrAdderMode, setIngrAdderMode] = useState(false);
@@ -83,7 +83,7 @@ function MyFridge({ onClose, isAuth }) {
   }, [currentIngr]);
 
   const getUserFridge = async () => {
-    if (isUser) {
+    if (isAuth) {
       try {
         const response = await requestApi("get", "/myfridge");
         setUserIngrData(response);
@@ -212,7 +212,193 @@ function MyFridge({ onClose, isAuth }) {
         </button>
       </Header>
       <Content>
-        {!isUser ? (
+        {isAuth && (
+          <>
+            {ingrAdderMode ? (
+              // 재료 추가하기 컴포넌트
+              <IngredientList>
+                <h4>
+                  <span>냉장고</span>를 채워보세요!
+                </h4>
+                {ingrData.map((group, index) => {
+                  return (
+                    <IngredientGroup key={index}>
+                      <h5>{group.type}</h5>
+                      <ul className="basicIngr">
+                        {group.ingredient.map((item, index) => {
+                          return (
+                            <li key={`ingr-${index}`}>
+                              <input
+                                type="checkbox"
+                                id={`ingr-${item.name}`}
+                                checked={item.selected}
+                                onChange={() => handleCheckboxChange(item)}
+                              />
+                              <label htmlFor={`ingr-${item.name}`}>
+                                <div className="box"></div>
+                                <span>{item.name}</span>
+                              </label>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </IngredientGroup>
+                  );
+                })}
+                <BtnGroup>
+                  <button
+                    className="cancelBtn"
+                    onClick={() => {
+                      setIngrAdderMode(false);
+                      setShowCurrentIngr(false);
+                      getUserFridge();
+                    }}
+                  >
+                    돌아가기
+                  </button>
+                  <button className="addBtn" onClick={addIngredient}>
+                    추가완료
+                  </button>
+                </BtnGroup>
+              </IngredientList>
+            ) : (
+              <>
+                {userIngrData.length > 0 ? (
+                  // 재료 있을 때
+                  <Fridge>
+                    <IngredientList>
+                      <IngredientGroup>
+                        <h5>나의 식재료</h5>
+                        <ul>
+                          {safeIngr.map((item) => {
+                            return (
+                              <li key={item._id}>
+                                <button onClick={() => handleCurrentIngr(item)}>
+                                  <div className="box"></div>
+                                  {item.ingredientName}
+                                </button>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </IngredientGroup>
+                      {spoiledIngr.length !== 0 && (
+                        <IngredientGroup>
+                          <h5>소비기한 마감 식재료</h5>
+                          <ul>
+                            {spoiledIngr.map((item) => {
+                              return (
+                                <li key={item._id}>
+                                  <button
+                                    onClick={() => handleCurrentIngr(item)}
+                                  >
+                                    {item.ingredientName}
+                                  </button>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </IngredientGroup>
+                      )}
+                      <button onClick={() => {}}>레시피 검색하기</button>
+                      <button
+                        onClick={() => {
+                          setIngrAdderMode(true);
+                          getUserFridge();
+                        }}
+                      >
+                        재료 추가하기
+                      </button>
+                    </IngredientList>
+                    {showCurrentIngr && (
+                      <div>
+                        <p>
+                          식재료:
+                          {currentIngr.ingredientName}
+                        </p>
+                        <p>
+                          식재료 추가일:
+                          {formatDate(currentIngr.inputDate)}
+                        </p>
+                        <p>
+                          소비기한 마감일:
+                          <button
+                            onClick={() => setShowCalendar((prev) => !prev)}
+                            disabled={
+                              new Date(currentIngr.bestBefore) < new Date()
+                            }
+                          >
+                            {newBestBefore
+                              ? formatDate(newBestBefore)
+                              : formatDate(currentIngr.bestBefore)}
+                          </button>
+                        </p>
+
+                        <PortalModal
+                          handleShowModal={showCalendar}
+                          size={"300px"}
+                        >
+                          <Calendar
+                            thisDate={new Date(currentIngr.bestBefore)}
+                            onThisDate={(date) => {
+                              setNewBestBefore(date);
+                            }}
+                            onThisClose={() => setShowCalendar(false)}
+                          />
+                        </PortalModal>
+                        {/* {showCalendar && (
+                    )} */}
+
+                        {new Date(currentIngr.bestBefore) > new Date() && (
+                          <button
+                            onClick={() => {
+                              updateIngredient(currentIngr._id, newBestBefore);
+                            }}
+                          >
+                            저장하기
+                          </button>
+                        )}
+                        <button
+                          onClick={() => {
+                            deleteIngredient(currentIngr._id);
+                          }}
+                        >
+                          삭제하기
+                        </button>
+                        <button
+                          onClick={() => {
+                            setShowCurrentIngr(false);
+                            setShowCalendar(false);
+                            setNewBestBefore();
+                            getUserFridge();
+                          }}
+                        >
+                          닫기
+                        </button>
+                      </div>
+                    )}
+                  </Fridge>
+                ) : (
+                  // 재료 없을 때
+                  <EmptyFridge>
+                    <img src={fridgeImg} alt="빈 식재료" height={230} />
+                    <h4>냉장고가 비었어요!</h4>
+                    <p>
+                      재료를 추가하고
+                      <br />
+                      바로 만들 수 있는 레시피를 확인 해보세요!
+                    </p>
+                    <button onClick={() => setIngrAdderMode(true)}>
+                      재료 추가하기
+                    </button>
+                  </EmptyFridge>
+                )}
+              </>
+            )}
+          </>
+        )}
+
+        {!isAuth && (
           <EmptyFridge>
             <h4>우리집 냉슐랭 요리사가 아니네요!</h4>
             <p>
@@ -224,179 +410,6 @@ function MyFridge({ onClose, isAuth }) {
               로그인하기
             </Link>
           </EmptyFridge>
-        ) : ingrAdderMode ? (
-          // 재료 추가하기 컴포넌트
-          <IngredientList>
-            <h4>
-              <span>냉장고</span>를 채워보세요!
-            </h4>
-            {ingrData.map((group, index) => {
-              return (
-                <IngredientGroup key={index}>
-                  <h5>{group.type}</h5>
-                  <ul className="basicIngr">
-                    {group.ingredient.map((item, index) => {
-                      return (
-                        <li key={`ingr-${index}`}>
-                          <input
-                            type="checkbox"
-                            id={`ingr-${item.name}`}
-                            checked={item.selected}
-                            onChange={() => handleCheckboxChange(item)}
-                          />
-                          <label htmlFor={`ingr-${item.name}`}>
-                            <div className="box"></div>
-                            <span>{item.name}</span>
-                          </label>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </IngredientGroup>
-              );
-            })}
-            <BtnGroup>
-              <button
-                className="cancelBtn"
-                onClick={() => {
-                  setIngrAdderMode(false);
-                  setShowCurrentIngr(false);
-                  getUserFridge();
-                }}
-              >
-                돌아가기
-              </button>
-              <button className="addBtn" onClick={addIngredient}>
-                추가완료
-              </button>
-            </BtnGroup>
-          </IngredientList>
-        ) : (
-          <>
-            {userIngrData.length > 0 ? (
-              // 재료 있을 때
-              <Fridge>
-                <IngredientList>
-                  <IngredientGroup>
-                    <h5>나의 식재료</h5>
-                    <ul>
-                      {safeIngr.map((item) => {
-                        return (
-                          <li key={item._id}>
-                            <button onClick={() => handleCurrentIngr(item)}>
-                              <div className="box"></div>
-                              {item.ingredientName}
-                            </button>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </IngredientGroup>
-                  {spoiledIngr.length !== 0 && (
-                    <IngredientGroup>
-                      <h5>소비기한 마감 식재료</h5>
-                      <ul>
-                        {spoiledIngr.map((item) => {
-                          return (
-                            <li key={item._id}>
-                              <button onClick={() => handleCurrentIngr(item)}>
-                                {item.ingredientName}
-                              </button>
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    </IngredientGroup>
-                  )}
-                  <button onClick={() => {}}>레시피 검색하기</button>
-                  <button
-                    onClick={() => {
-                      setIngrAdderMode(true);
-                      getUserFridge();
-                    }}
-                  >
-                    재료 추가하기
-                  </button>
-                </IngredientList>
-                {showCurrentIngr && (
-                  <div>
-                    <p>
-                      식재료:
-                      {currentIngr.ingredientName}
-                    </p>
-                    <p>
-                      식재료 추가일:
-                      {formatDate(currentIngr.inputDate)}
-                    </p>
-                    <p>
-                      소비기한 마감일:
-                      <button
-                        onClick={() => setShowCalendar((prev) => !prev)}
-                        disabled={new Date(currentIngr.bestBefore) < new Date()}
-                      >
-                        {newBestBefore
-                          ? formatDate(newBestBefore)
-                          : formatDate(currentIngr.bestBefore)}
-                      </button>
-                    </p>
-
-                    <PortalModal handleShowModal={showCalendar} size={"300px"}>
-                      <Calendar
-                        thisDate={new Date(currentIngr.bestBefore)}
-                        onThisDate={(date) => {
-                          setNewBestBefore(date);
-                        }}
-                        onThisClose={() => setShowCalendar(false)}
-                      />
-                    </PortalModal>
-                    {/* {showCalendar && (
-                    )} */}
-
-                    {new Date(currentIngr.bestBefore) > new Date() && (
-                      <button
-                        onClick={() => {
-                          updateIngredient(currentIngr._id, newBestBefore);
-                        }}
-                      >
-                        저장하기
-                      </button>
-                    )}
-                    <button
-                      onClick={() => {
-                        deleteIngredient(currentIngr._id);
-                      }}
-                    >
-                      삭제하기
-                    </button>
-                    <button
-                      onClick={() => {
-                        setShowCurrentIngr(false);
-                        setShowCalendar(false);
-                        setNewBestBefore();
-                        getUserFridge();
-                      }}
-                    >
-                      닫기
-                    </button>
-                  </div>
-                )}
-              </Fridge>
-            ) : (
-              // 재료 없을 때
-              <EmptyFridge>
-                <img src={fridgeImg} alt="빈 식재료" height={230} />
-                <h4>냉장고가 비었어요!</h4>
-                <p>
-                  재료를 추가하고
-                  <br />
-                  바로 만들 수 있는 레시피를 확인 해보세요!
-                </p>
-                <button onClick={() => setIngrAdderMode(true)}>
-                  재료 추가하기
-                </button>
-              </EmptyFridge>
-            )}
-          </>
         )}
       </Content>
     </>
