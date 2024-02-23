f8df45a0942d8fd50925064ce03be0aa7a40b1ff
LEE SUL <suerish.e@gmail.com>
2024-02-23 17:45:09 +0900

    modify: api base url 환경변수 적용

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,10 +3,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:3001",
+      target: process.env.REACT_APP_API_URL || "http://localhost:3001",
       changeOrigin: true,
     })
   );
 };
-// - ip : 34.64.93.95
-// - domain : kdt-sw-5-2-team05.elicecoding.com
