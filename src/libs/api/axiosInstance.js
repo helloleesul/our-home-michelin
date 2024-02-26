18248ecc39beadafe3eac5b0acae4aad3fcba2d1
LEE SUL <suerish.e@gmail.com>
2024-02-26 21:34:23 +0900

    refactor: api instance, 공통 함수

---
+++ src/libs/api/axiosInstance.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  // setupProxy target설정으로 baseURL의 http://localhost:3001 제외하였음
+  baseURL: "/api",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 요청 인터셉터 추가
+axiosInstance.interceptors.request.use(
+  (request) => {
+    // 요청을 보내기 전에 수행할 작업을 여기에 추가할 수 있습니다.
+    // 예: 헤더 수정, 요청 데이터 변경 등
+    if (request.data instanceof FormData) {
+      request.headers["Content-Type"] = "multipart/form-data";
+    }
+    return request;
+  },
+  (error) => {
+    // 요청이 실패한 경우에 대한 처리
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 추가
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // 응답을 받은 후 수행할 작업을 여기에 추가할 수 있습니다.
+    // 예: 응답 데이터 수정, 오류 처리 등
+    return response;
+  },
+  (error) => {
+    // 응답이 오류인 경우에 대한 처리
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
---
+++ src/libs/api/index.js
@@ -0,0 +1,39 @@
+import axiosInstance from "./axiosInstance";
+
+const sendRequest = async (method, url, data = null) => {
+  try {
+    const response = await axiosInstance({
+      method,
+      url,
+      data,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// GET 요청을 보내는 함수
+export const GET = async (url) => {
+  return await sendRequest("get", url);
+};
+
+// POST 요청을 보내는 함수
+export const POST = async (url, data) => {
+  return await sendRequest("post", url, data);
+};
+
+// PUT 요청을 보내는 함수
+export const PUT = async (url, data) => {
+  return await sendRequest("PUT", url, data);
+};
+
+// PATCH 요청을 보내는 함수
+export const PATCH = async (url, data) => {
+  return await sendRequest("patch", url, data);
+};
+
+// DELETE 요청을 보내는 함수
+export const DELETE = async (url) => {
+  return await sendRequest("delete", url);
+};
