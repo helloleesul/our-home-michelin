4082a4f3845e0ab9e9e2979c0553698e96e3dede
LEE SUL <suerish.e@gmail.com>
2024-02-27 17:49:46 +0900

    add: 공통 select 생성

---
+++ src/components/common/Select/index.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function Select({ options, onChange, defaultMessage }) {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+    onChange(e.target.value);
+  };
+
+  return (
+    <select value={selectedOption} onChange={handleOptionChange}>
+      <option value="">{defaultMessage}</option>
+      {options.map((option) => (
+        <option key={option.value} value={option.label}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+}
