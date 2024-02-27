a31226d6b14b107e6366da14b13445ff51ffa720
LEE SUL <suerish.e@gmail.com>
2024-02-27 19:20:07 +0900

    update: 공통 button onClick props

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,9 +1,9 @@
 import * as S from "./style";
 
-export default function Button({ type, value, disabled, width }) {
+export default function Button({ type, value, disabled, width, onClick }) {
   // type: "submit" | "reset" | "button"
   return (
-    <S.Button type={type} disabled={disabled} width={width}>
+    <S.Button type={type} disabled={disabled} width={width} onClick={onClick}>
       {value}
     </S.Button>
   );
