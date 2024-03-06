0950605562ee48678eb5a648902553eb9769d04c
LEE SUL <suerish.e@gmail.com>
2024-03-07 00:44:35 +0900

    modify: 회원 접근 라우터 alert로 수정

--- src/components/router/UserRoute/index.js
+++ src/components/router/UserRoute/index.js
@@ -1,13 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectAuth } from "@/libs/store/authSlice";
+import { useEffect } from "react";
+import MESSAGE from "@/libs/constants/message";
 
 export default function UserRoute() {
   const { isAuthenticated } = useSelector(selectAuth);
+  const navigate = useNavigate();
 
-  if (!isAuthenticated) {
-    return <>로그인 후 이용하세요</>;
-  } else {
+  useEffect(() => {
+    if (!isAuthenticated) {
+      alert(MESSAGE.LOGIN.REQUIRED);
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate]);
+
+  if (isAuthenticated) {
     return <Outlet />;
+  } else {
+    return null;
   }
 }
