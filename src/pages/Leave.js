05b860944f2a28fc47dc012c7ea609f8560b5176
LEE SUL <suerish.e@gmail.com>
2024-03-28 01:18:38 +0900

    design: 회원 탈퇴

--- src/pages/Leave.js
+++ src/pages/Leave.js
@@ -1,20 +1,22 @@
 import Title from "@/components/common/Title";
 import ConfirmForm, { Mode } from "@/components/user/ConfirmForm";
 import UserForm from "@/components/user/UserForm";
-import { Flex } from "@/styles/common";
+import { Center, Flex } from "@/styles/common";
 
 export default function Leave() {
   return (
-    <Flex gap={"30"}>
-      <Title
-        icon={"🚪🏃‍♀️"}
-        title={"회원 탈퇴"}
-        type={"primary"}
-        position={"center"}
-      />
-      <UserForm form={<ConfirmForm mode={Mode.LEAVE} />}>
-        <p>우리집 냉슐랭을 탈퇴하시려면 본인 확인이 필요합니다.</p>
-      </UserForm>
-    </Flex>
+    <Center>
+      <Flex gap={"30"}>
+        <Title
+          icon={"🚪🏃‍♀️"}
+          title={"회원 탈퇴"}
+          type={"primary"}
+          position={"center"}
+        />
+        <UserForm form={<ConfirmForm mode={Mode.LEAVE} />}>
+          <p>우리집 냉슐랭을 탈퇴하시려면 본인 확인이 필요합니다.</p>
+        </UserForm>
+      </Flex>
+    </Center>
   );
 }
