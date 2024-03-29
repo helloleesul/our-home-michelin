2b1dae554e9b349369cd263b7d29012775a510dc
LEE SUL <suerish.e@gmail.com>
2024-03-29 16:48:20 +0900

    modify: 배포용 axiosInstance baseURL

--- src/libs/api/axiosInstance.js
+++ src/libs/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: "/api",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
